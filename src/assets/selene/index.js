46cce9e6b2bab2bef4d125d73190ee1c29801503
Jellystump <little.jelly.stump@gmail.com>
2026-09-17 10:16:54 -0400

    changed selene project photos

---
+++ src/assets/selene/index.js
@@ -0,0 +1,4 @@
+export { default as selene1 } from './selene1.jpeg';
+export { default as selene2 } from './selene2.jpeg';
+export { default as selene3 } from './selene3.jpeg';
+export { default as selene4 } from './selene4.jpeg';
